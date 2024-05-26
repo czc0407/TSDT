a63c1571847636625f7ba8e0bafc57c91b73e6e5
czc0407 <1532886617@qq.com>
2024-05-26 16:15:34 +0800

    message 24: move static file and database outside of main source tree

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -13,7 +13,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        real_server = os.environ.get('REAL_SERVER')
+        real_server = os.environ.get('8.130.174.100')
         if real_server:
             self.live_server_url = 'http://' + real_server
     
